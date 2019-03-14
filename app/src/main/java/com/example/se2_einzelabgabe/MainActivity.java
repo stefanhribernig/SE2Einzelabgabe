0dd0636158fbe9217456072d53574a1a7a5031f8
Stefan Hribernig <stefanhr@edu.aau.at>
2019-03-14 09:34:17 +0100

    Methode zur Berechnung der Primzahlen aus der Matrikelnummer hinzugefügt

--- app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     String modifiedSentence;
     String mnrt;
     clientSocket cSocket;
+    String prim;
+    String primFinal = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +44,49 @@ public class MainActivity extends AppCompatActivity {
     public void setAntwort(String s){
         antwort.setText(s);
     }
+
+    public void calculate(View v){
+        mnr = this.findViewById(R.id.MNrID);
+        antwort = this.findViewById(R.id.antwortID);
+        primFinal = "";
+
+        prim = mnr.getText().toString();
+
+
+        String[] arrayString = prim.split("");
+        int[] arrayInt = new int[arrayString.length];
+
+
+
+        for (int i = 1; i < arrayString.length; i++) {
+            Log.i("Erstes Element", arrayString[i]);
+            arrayInt[i] = Integer.valueOf(arrayString[i]);
+        }
+
+
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            boolean isPrimzahl = true;
+            for (int j = 2; j < arrayInt[i]; j++) {
+                if((arrayInt[i]%j) == 0){
+                    isPrimzahl = false;
+                }
+            }
+            if(isPrimzahl && arrayInt[i] != 0 && arrayInt[i] != 1){
+                primFinal += arrayString[i];
+            }
+        }
+
+        if(primFinal == ""){
+            setAntwort("Es ist keine Primzahl enthalten");
+        } else {
+            setAntwort(primFinal);
+        }
+
+
+
+    }
+
 
     class clientSocket implements Runnable{
 
